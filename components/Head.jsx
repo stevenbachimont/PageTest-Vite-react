0c90582f0aaaed10e5fdcae345e473bf94b3c417
STEVEN BACHIMONT <stevenbachimont@gmail.com>
2024-03-26 16:58:34 +0100

    push

---
+++ components/Head.jsx
@@ -0,0 +1,17 @@
+import "/components/Head.css"
+
+function Head() {
+    return(
+        <nav>
+            <h1>Wow !</h1>
+            <ul>
+                <li>Bonjour</li>
+                <li>Salut</li>
+                <li>Merci</li>
+            </ul>
+        </nav>
+    )
+
+}
+
+export default Head;
---
+++ components/Main.jsx
@@ -0,0 +1,12 @@
+import "/components/Main.css"
+
+function Main() {
+    return(
+        <>
+            <img src="https://media3.giphy.com/avatars/doodlesbyburnttoast/dMqxHmPPA8fd.gif" alt="urk"/>
+        </>
+    )
+
+}
+
+export default Main;
